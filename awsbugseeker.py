1c71ca0ca2a7d04626b46329c846a5f9c4f7b9c4
Shockz OffSec <67438760+shockz-offsec@users.noreply.github.com>
2023-04-10 21:57:22 +0200

    Update awsbugseeker.py
    Buenas, te sugiero de cara a hacer tu código más optimo lo siguiente:
    
    En lugar de hacer una lista de hilos y esperar a que se ejecuten en un ciclo, podemos usar una cosa llamada "ThreadPoolExecutor" del módulo "concurrent.futures" para ejecutar las funciones de escaneo en paralelo.
    
    También podemos utilizar una lista de tuplas para almacenar las funciones de escaneo y sus argumentos en lugar de crear una función para cada una. Esto hará que nuestro código sea más flexible y corto.
    
    En lugar de imprimir los resultados en la consola y escribirlos en un archivo al mismo tiempo, podemos almacenar los resultados en un diccionario. De esta manera, podemos procesar los resultados antes de escribirlos en un archivo y hacer que nuestro código sea más fácil de leer y mantener.
    
    Espero que te ayude!! Saludos.

--- awsbugseeker.py
+++ awsbugseeker.py
@@ -1,6 +1,6 @@
 import argparse
+import concurrent.futures
 import subprocess
-import threading
 
 # Definimos los argumentos de línea de comandos
 parser = argparse.ArgumentParser(description='Escáner de vulnerabilidades para aplicaciones y entornos montados en AWS.')
@@ -8,50 +8,48 @@ parser.add_argument('--profile', type=str, help='perfil de AWS CLI')
 parser.add_argument('--output', type=str, default='txt', choices=['txt', 'csv'], help='formato de salida de resultados')
 args = parser.parse_args()
 
-# Escaneamos las instancias EC2
+# Funciones de escaneo
 def scan_ec2():
-    subprocess.run(['aws', 'ec2', 'describe-instances'], check=True)
+    return subprocess.run(['aws', 'ec2', 'describe-instances'], check=True, capture_output=True, text=True).stdout
 
-# Escaneamos los grupos de destino de balanceadores de carga
 def scan_elb():
-    subprocess.run(['aws', 'elbv2', 'describe-target-groups'], check=True)
+    return subprocess.run(['aws', 'elbv2', 'describe-target-groups'], check=True, capture_output=True, text=True).stdout
 
-# Escaneamos las instancias de bases de datos RDS
 def scan_rds():
-    subprocess.run(['aws', 'rds', 'describe-db-instances'], check=True)
+    return subprocess.run(['aws', 'rds', 'describe-db-instances'], check=True, capture_output=True, text=True).stdout
 
-# Escaneamos utilizando PACU
 def scan_pacu():
-    subprocess.run(['pacu', '-t', 'run', '-r', 'all'], check=True)
+    return subprocess.run(['pacu', '-t', 'run', '-r', 'all'], check=True, capture_output=True, text=True).stdout
 
-# Escaneamos utilizando Prowler
 def scan_prowler():
-    subprocess.run(['prowler', '-M', 'cis'], check=True)
+    return subprocess.run(['prowler', '-M', 'cis'], check=True, capture_output=True, text=True).stdout
 
-# Creamos una lista de hilos para ejecutar los escaneos en paralelo
-threads = []
-threads.append(threading.Thread(target=scan_ec2))
-threads.append(threading.Thread(target=scan_elb))
-threads.append(threading.Thread(target=scan_rds))
-threads.append(threading.Thread(target=scan_pacu))
-threads.append(threading.Thread(target=scan_prowler))
+# Lista de tuplas con funciones de escaneo y argumentos
+scan_functions = [
+    (scan_ec2, ()),
+    (scan_elb, ()),
+    (scan_rds, ()),
+    (scan_pacu, ()),
+    (scan_prowler, ())
+]
 
-# Ejecutamos el escaneo
-for thread in threads:
-    thread.start()
-for thread in threads:
-    thread.join()
+# Ejecutamos las funciones de escaneo en paralelo utilizando un ThreadPoolExecutor
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = executor.map(lambda x: x[0](*x[1]), scan_functions)
 
-# Imprimimos los resultados
-print('\nResultados del escaneo:\n')
-# Aquí se podrían procesar e imprimir los resultados de una forma más clara y legible, por ejemplo, utilizando colores para resaltar la gravedad de las vulnerabilidades encontradas
+# Procesamos los resultados y los almacenamos en un diccionario
+scan_results = {}
+for i, result in enumerate(results):
+    scan_results[f'Escaneo {i+1}'] = result.splitlines()
 
-# Exportamos los resultados en un archivo
-output_file = 'resultados_escaneo.' + args.output
+# Escribimos los resultados en un archivo
+output_file = f'resultados_escaneo.{args.output}'
 with open(output_file, 'w') as f:
-    # Aquí se podrían exportar los resultados en diferentes formatos, dependiendo de lo especificado en los argumentos
-    f.write('Resultados del escaneo:\n')
-    f.write('------------------------\n')
-    f.write('\n')  # Aquí se podría escribir los resultados de forma organizada
+    for key, value in scan_results.items():
+        f.write(f'{key}:\n')
+        f.write('------------------------\n')
+        for line in value:
+            f.write(f'{line}\n')
+        f.write('\n')
 
 print(f'Los resultados del escaneo se han exportado en el archivo "{output_file}".')
